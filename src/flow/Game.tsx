8a6857120cc649f57324252387d34f99f7ad77e9
Mateusz Kasprzak (B2B CPL) <mateusz.kasprzak.ext@coconet.pl>
2025-08-14 14:53:09 +0200

    tsc

--- src/flow/Game.tsx
+++ src/flow/Game.tsx
@@ -1,14 +1,7 @@
-import { useWhiteboardState } from "../useWhiteboardState.ts";
-import { useNavigate } from "react-router";
 import styles from "./Game.module.css";
 import { SpaceBackground } from "../components/SpaceBackground.tsx";
-import { useEffect } from "react";
-import { useState } from "react";
 
 export function Game() {
-  const state = useWhiteboardState();
-  const navigate = useNavigate();
-
   return (
     <SpaceBackground className={styles.wrapper}>
       <h1>Game!</h1>
