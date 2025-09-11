8fced1fb212d32674d2d7dd71fd0e540ce5289ed
Mateusz Kasprzak (B2B CPL) <mateusz.kasprzak.ext@coconet.pl>
2025-09-11 11:35:56 +0200

    update default ws server

--- src/consts.client.ts
+++ src/consts.client.ts
@@ -1,2 +1,2 @@
-export const WHITEBOARD_WEBSOCKET_URL = localStorage.getItem("WS_URL") ?? "ws://localhost:8123";
+export const WHITEBOARD_WEBSOCKET_URL = localStorage.getItem("WS_URL") ?? "wss://192.168.0.2:8765";
 export const IS_MOCK = localStorage.getItem("MOCK") === "true";
