648fe0e7a81c98085c29c1df920bee1ecf77dfad
Mateusz Kasprzak (B2B CPL) <mateusz.kasprzak.ext@coconet.pl>
2025-09-16 18:13:59 +0200

    update sprites and add game play tutorial

--- src/flow/GameIntro.tsx
+++ src/flow/GameIntro.tsx
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
 import { SpaceBackground } from "../components/SpaceBackground.tsx";
+import styles from "./Trailer.module.css";
 import { useNavigate } from "react-router";
 
 export default function GameIntro() {
   const navigate = useNavigate();
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      navigate("instruction", { viewTransition: true });
-    }, 2000);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [navigate]);
-  return <SpaceBackground type="gameplay">Here we will play a game intro and after it ends....</SpaceBackground>;
+  return (
+    <SpaceBackground type="gameplay" contentClassName={styles.trailer}>
+      <video
+        src="how-to-play.mp4"
+        autoPlay
+        controls={false}
+        onEnded={() => navigate("instruction", { viewTransition: true })}
+      ></video>
+    </SpaceBackground>
+  );
 }
